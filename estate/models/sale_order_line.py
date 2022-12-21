c23176698c1698a3d00b23d77e1086afe9411b41
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-21 14:19:59 +0100

    commit

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,4 +2,4 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-  
+    training_date = fields.Date(string="Training date") #attention revoir le type
