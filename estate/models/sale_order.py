325ee54f7a9ea9e77bbc950335f6e21eb3ad6c0a
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-21 20:32:53 +0100

    some changes ...

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -7,16 +7,15 @@ class SaleOrder(models.Model):
     
     def action_confirm(self):
         # Call the super method to confirm the sale order
-        res = super(SaleOrder, self).action_confirm()
 
         for line in self.order_line:
            if line.training_date and line.employee_id:
                 event_vals = {
                     'name': 'Training',
                     'start_date': line.training_date,
-                    'stop_date': line.training_date,
+                    'stop_date': line.training_date + timedelta(8),
                     'partner_ids': [(4, line.employee_id.id)],
                 }
                 self.env['calendar.event'].create(event_vals)    
 
-        return res
+        return super(SaleOrder, self).action_confirm()
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,6 +3,6 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training date") #attention revoir le type
-    employee_id = fields.Many2one(comodel_name='hr.employee', string='Employee')
+    employee_id = fields.Many2one('hr.employee', string='Employee')
 
             
