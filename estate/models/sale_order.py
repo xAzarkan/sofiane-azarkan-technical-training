b2e14590014280c9c1f51ec79d7f9b53ccc00c06
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-21 21:39:43 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -19,7 +19,7 @@ class SaleOrder(models.Model):
                     'start_date': line.training_date,
                     'stop_date': line.training_date + timedelta(hours=8),
                     'allday': True,
-                    'attendee_ids': [(0, line.employee_id.id)], #changer cela pour récuperer l'employé selectionné
+                    'partner_ids': [(6, 0, [line.employee_id.address_home_id.id])], #changer cela pour récuperer l'employé selectionné
                 })
 
         return res
