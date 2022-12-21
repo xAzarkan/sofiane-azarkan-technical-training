9761610cf5c3418a949b51497e063269c1bc2396
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-21 16:12:21 +0100

    Update sale_order_line.py

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -4,3 +4,22 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training date") #attention revoir le type
     employee_id = fields.Many2one(comodel_name='hr.employee', string='Employee')
+
+    @api.multi
+    def confirm(self):
+        # Call the original confirm method to complete the sale order confirmation process
+        super().confirm()
+
+        # Retrieve the calendar of the selected employee
+        employee = self.employee_id
+        calendar = employee.resource_calendar_id
+
+        # Create a new event in the calendar
+        event_vals = {
+            'name': self.name,
+            'start': self.date_order,
+            'stop': self.date_order + timedelta(hours=8),
+            'allday': False,
+            'resource_id': calendar.id,
+        }
+        event = self.env['calendar.event'].create(event_vals)
