ebcb280e36e46fde40c677f34feb92dc70875c92
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-21 14:22:14 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -3,4 +3,4 @@ from odoo.exceptions import ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    training_date = fields.Date(string="Training date") #attention revoir le type
+   
