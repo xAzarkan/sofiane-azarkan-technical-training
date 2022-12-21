3769eb2ad01105a2e84be79e0ab571781bb6b0d5
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-21 16:54:37 +0100

    add confirm method in sale order

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -4,4 +4,19 @@ from odoo.exceptions import ValidationError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    
+    def confirm(self):
+        # Call the super method to confirm the sale order
+        super(SaleOrder, self).confirm()
+
+        # Iterate over the sale order lines
+        for line in self.order_line:
+            # Check if the training date is set
+            if line.training_date:
+                # Create a new event in the calendar for the selected employee
+                self.env['calendar.event'].create({
+                    'name': 'Training',
+                    'start_date': line.training_date,
+                    'stop_date': line.training_date + timedelta(hours=8),
+                    'allday': True,
+                    'partner_ids': [(6, 0, [self.partner_id.id])],
+                })
