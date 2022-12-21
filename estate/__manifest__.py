2fdb4fc63bab35f48e4c5fcddc587b3371c5f324
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-21 22:16:49 +0100

    V2.0

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Sofiane Azarkan",
     'website': "http://www.yourcompany.com",
     'category': 'Uncategorized',
-    'version': '16.0.0.1',
+    'version': '16.0.0.2',
     'license': 'LGPL-3',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
     'data': ['views/res_groups.xml', 'views/res_partner.xml', 'views/sale_order.xml']
