600c19053afd635c8c4d816f13490af7f0b6e81b
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-22 21:03:48 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -4,34 +4,54 @@ from datetime import timedelta
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    
     
     def action_confirm(self):
+        # Get the current connected user
         current_user = self.env.user
-        self.message_post(body=f'Sale order confirmed by {current_user.name}')
 
-        # Call the super method to confirm the sale order
-        res = super(SaleOrder, self).action_confirm()
+        max_amount = get_max_amount_value(self, 'Manager Level 2')
 
-        # Iterate over the sale order lines
-        for line in self.order_line:
+        if self.amount_total <= max_amount:
+            # Iterate over the sale order lines
+            for line in self.order_line:
+                
+                # Création d'un partenaire avec le nom de l'employé s'il n'a pas de partenaire associé
+                if not line.employee_id.user_id:
+                    partner = self.env['res.partner'].create({
+                        'name': line.employee_id.name,
+                    })
+                else:
+                    partner = line.employee_id.user_id.partner_id
+
+                # Check if the training date is set
+                if line.training_date:
+                    # Create a new event in the calendar for the selected employee
+                    self.env['calendar.event'].create({
+                        'name': 'Training for sale order',
+                        'start_date': line.training_date,
+                        'stop_date': line.training_date + timedelta(hours=8),
+                        'allday': True,
+                        'partner_ids': [(4, partner.id)],
+                    })
+
+            # Call the super method to confirm the sale order
+            return super(SaleOrder, self).action_confirm()
             
-            # Création d'un partenaire avec le nom de l'employé s'il n'a pas de partenaire associé
-            if not line.employee_id.user_id:
-                partner = self.env['res.partner'].create({
-                    'name': line.employee_id.name,
-                })
-            else:
-                partner = line.employee_id.user_id.partner_id
+        else:
+            return self.message_post(body=f'Sale order can not be confirmed by {current_user.name}')
 
-            # Check if the training date is set
-            if line.training_date:
-                # Create a new event in the calendar for the selected employee
-                self.env['calendar.event'].create({
-                    'name': 'Training for sale order',
-                    'start_date': line.training_date,
-                    'stop_date': line.training_date + timedelta(hours=8),
-                    'allday': True,
-                    'partner_ids': [(4, partner.id)],
-                })
 
-        return res
+    def get_max_amount_value(self, group_name):
+        # Search for the group by name
+        group_ids = self.env['res.groups'].search([('name', '=', group_name)])
+        # Get the group record
+        group = self.env['res.groups'].browse(group_ids)
+        # Get the value of the field
+        return group.max_amount
+        # Do something with the field value
+        #print(field_value)
+
+
+    
