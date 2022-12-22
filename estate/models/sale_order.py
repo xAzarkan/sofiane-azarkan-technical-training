1694847afc2f05525027ab51f5fe4bca0d316f4c
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-22 23:07:07 +0100

    Partner feature max amount v2

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -15,30 +15,33 @@ class SaleOrder(models.Model):
         max_amount = self.get_max_amount_value()
 
         # Verify max amount of the group AND max amount of the partner
-        if self.amount_total <= max_amount and self.amount_total <= partner.max_sale_order_amount or partner.max_sale_order_amount is None:
-            # Iterate over the sale order lines
-            for order in self.order_line:
-                # Création d'un partenaire avec le nom de l'employé s'il n'a pas de partenaire associé
-                if not order.employee_id.user_id:
-                    partner = self.env['res.partner'].create({
-                        'name': order.employee_id.name,
-                    })
-                else:
-                    partner = order.employee_id.user_id.partner_id
+        if self.amount_total <= max_amount: 
+            if self.amount_total <= partner.max_sale_order_amount or partner.max_sale_order_amount is None:
+                # Iterate over the sale order lines
+                for order in self.order_line:
+                    # Création d'un partenaire avec le nom de l'employé s'il n'a pas de partenaire associé
+                    if not order.employee_id.user_id:
+                        partner = self.env['res.partner'].create({
+                            'name': order.employee_id.name,
+                        })
+                    else:
+                        partner = order.employee_id.user_id.partner_id
 
-                # Check if the training date is set
-                if order.training_date:
-                    # Create a new event in the calendar for the selected employee
-                    self.env['calendar.event'].create({
-                        'name': 'Training for sale order',
-                        'start_date': order.training_date,
-                        'stop_date': order.training_date + timedelta(hours=8),
-                        'allday': True,
-                        'partner_ids': [(4, partner.id)],
-                    })
+                    # Check if the training date is set
+                    if order.training_date:
+                        # Create a new event in the calendar for the selected employee
+                        self.env['calendar.event'].create({
+                            'name': 'Training for sale order',
+                            'start_date': order.training_date,
+                            'stop_date': order.training_date + timedelta(hours=8),
+                            'allday': True,
+                            'partner_ids': [(4, partner.id)],
+                        })
 
-            # Call the super method to confirm the sale order
-            return super(SaleOrder, self).action_confirm()
+                # Call the super method to confirm the sale order
+                return super(SaleOrder, self).action_confirm()
+            else:
+                return self.message_post(body=f'Sale order can not be confirmed for the partner {partner.name}')    
 
         else:
             return self.message_post(body=f'Sale order can not be confirmed by {current_user.name}')
