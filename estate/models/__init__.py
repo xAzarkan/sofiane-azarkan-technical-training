67bd2f3c5d0355aeffdd699df4aa61831ff1d35f
Sofiane Azarkan <sofiane.azarkan@gmail.com>
2022-12-24 20:43:47 +0100

    Import res_users

--- estate/models/__init__.py
+++ estate/models/__init__.py
@@ -4,3 +4,4 @@ from . import sale_order_line
 from . import sale_order
 from . import res_groups
 from . import res_partner
+from . import res_users
--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -2,6 +2,6 @@ from odoo import fields, models
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
-    
+
     #Pour le premier bonus : auto assign manager : every time a user approve a sale.order, we can increment a integer field
     counter_approved_sale_order = fields.Integer(string="Number of approved sale order")
